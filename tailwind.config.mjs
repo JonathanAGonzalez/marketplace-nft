d00b5312829893eb29adcce26bb0d6fb8c104736
Jonas <jonathan.gonzalez@gm2dev.com>
2024-01-14 13:24:10 -0300

    feat(plugins tailwind): add plugin flowbite and colors tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -8,8 +11,10 @@ export default {
       },
       colors: {
         'primary-purple': '#9d6be5',
+        'blue-strong': '#10193b',
+        'blue-light': '#6a86ff',
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };

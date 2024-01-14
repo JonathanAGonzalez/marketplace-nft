a1dfc292c7fe1c6463d532e9229a05da35bb0293
Jonas <jonathan.gonzalez@gm2dev.com>
2024-01-14 13:28:38 -0300

    feat(ButtonCircleHover): add button with animation

---
+++ src/components/atomic/atoms/buttons/ButtonWithCircleHover.tsx
@@ -0,0 +1,16 @@
+type ButtonWithHoverProps = {
+  text: string;
+};
+
+export const ButtonWithCircleHover = ({ text }: ButtonWithHoverProps) => {
+  return (
+    <button className='relative border hover:border-blue-600 duration-500 group cursor-pointer text-sky-50  overflow-hidden h-14 w-56 rounded-md bg-blue-800 p-2 flex justify-center items-center font-extrabold'>
+      <div className='absolute z-10 w-48 h-48 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-blue-900 delay-150 group-hover:delay-75'></div>
+      <div className='absolute z-10 w-40 h-40 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-blue-800 delay-150 group-hover:delay-100'></div>
+      <div className='absolute z-10 w-32 h-32 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-blue-700 delay-150 group-hover:delay-150'></div>
+      <div className='absolute z-10 w-24 h-24 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-blue-600 delay-150 group-hover:delay-200'></div>
+      <div className='absolute z-10 w-16 h-16 rounded-full group-hover:scale-150 transition-all  duration-500 ease-in-out bg-blue-500 delay-150 group-hover:delay-300'></div>
+      <p className='z-10'>{text}</p>
+    </button>
+  );
+};

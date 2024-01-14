b244cf841eb161d376d1d4e96787c48239a6330e
Jonas <jonathan.gonzalez@gm2dev.com>
2024-01-14 13:25:40 -0300

    feat(Link): add component link

--- src/components/atomic/atoms/Link.tsx
+++ src/components/atomic/atoms/Link.tsx
@@ -1,13 +1,23 @@
 type TLinkProps = {
   children: React.ReactNode;
   href: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+  aditonalClass?: string;
 };
-export const Link = ({ children, href, ...rest }: TLinkProps) => {
+
+export const Link = ({
+  children,
+  href,
+  onClick,
+  aditonalClass,
+  ...rest
+}: TLinkProps) => {
   return (
     <a
       href={href}
       {...rest}
-      className='text-white font-medium hover:text-primary-purple transition-all duration-500'
+      onClick={onClick}
+      className={`text-white font-medium hover:text-primary-purple transition-all duration-500 ${aditonalClass}`}
     >
       {children}
     </a>

62f64e0335cb6e5826f5ec1d9f6f5ffb3d694a9b
Jona <Jonathang@hotmail.com.ar>
2024-01-24 10:56:07 -0300

    feat(DetailProfileOwner): add prop to get text alternative

--- src/components/atomic/molecules/Detail/DetailProfileOwner.tsx
+++ src/components/atomic/molecules/Detail/DetailProfileOwner.tsx
@@ -2,20 +2,19 @@ import { ProfileImage } from '../../atoms/profile/ProfileImage';
 import { OwnerName } from '../../atoms/owner/OwnerName';
 
 type TDetailProfileOwnerProps = {
-  image: {
-    src: string;
-    alt: string;
-  };
+  image: string;
+  alt: string;
   owner: string;
 };
 
 export const DetailProfileOwner = ({
   image,
+  alt,
   owner,
 }: TDetailProfileOwnerProps) => {
   return (
     <div className='flex items-center gap-4'>
-      <ProfileImage alt={image.alt} src={image.src} />
+      <ProfileImage alt={alt} src={image} />
       <OwnerName owner={owner} />
     </div>
   );

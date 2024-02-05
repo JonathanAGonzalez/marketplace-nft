d8a41f304e208f2447232b1088d5185607588a6e
Jona <jonathang@hotmail.com.ar>
2024-02-04 21:45:16 -0300

    refactor: remove initWeb3AuthModal

--- src/components/atomic/organisms/Header.tsx
+++ src/components/atomic/organisms/Header.tsx
@@ -1,16 +1,7 @@
-import { useEffect } from 'react';
 import { Logo } from '../atoms/Logo';
 import { Menu } from './Menu';
-import { useAuthStore } from '../../../stores/auth.store';
 
 export const Header = () => {
-  const initWeb3Auth = useAuthStore((store) => store.initWeb3auth);
-
-  useEffect(() => {
-    (async () => {
-      initWeb3Auth();
-    })();
-  }, []);
   return (
     <header className='flex items-center justify-between p-6'>
       <Logo />
